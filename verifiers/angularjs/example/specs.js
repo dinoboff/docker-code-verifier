af9021224faa4f74798c0a6d44a275dbe7f2f31a
Damien Lebrun <dinoboff@hotmail.com>
2015-04-26 22:44:05 +0100

    Reproduce bug with angular verifier

--- verifiers/angularjs/example/specs.js
+++ verifiers/angularjs/example/specs.js
@@ -4,6 +4,14 @@ describe('angularjs ng-controller', function() {
 
     expect(
       element(by.binding('ctrl.name')).getText()
+    ).toBe('Hello Alice!');
+  });
+
+  it('should say "hello Bob"', function() {
+    browser.get('./');
+
+    expect(
+      element(by.binding('ctrl.name')).getText()
     ).toBe('Hello Bob!');
   });
 });
